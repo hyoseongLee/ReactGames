bacf9eef6ac40724c36c02d652df84cb7f2b9402
hyoseong <hyoseong123@naver.com>
2025-10-17 20:46:40 +0900

    refactor: ui 수정 및 이미지 수정정

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="descrition">
-        <h3>" React 마스터가 될때까지! " </h3>
+        <h3>"React 마스터가 될때까지! " </h3>
         <p>현재 개발 단계 중입니다.</p>
       </div>
       <GameList games={games} />
